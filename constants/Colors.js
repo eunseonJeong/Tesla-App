e3e7dbb676ad01166a722b98c5e68b52b5afe004
정은선 <suni980215@gmail.com>
2024-05-22 16:17:55 +0900

    feat: 색상파일 추가

---
+++ constants/Colors.js
@@ -0,0 +1,26 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = "#0a7ea4";
+
+const primary = "#42E2B8";
+const blue = "#07004D";
+const gray = "#898989";
+const white = "#ffffff";
+const black = "#000000";
+
+export const Colors = {
+  text: "#11181C",
+  background: "#fff",
+  tint: tintColorLight,
+  icon: "#687076",
+  tabIconDefault: "#687076",
+  tabIconSelected: tintColorLight,
+  primary: primary,
+  blue: blue,
+  gray: gray,
+  white: white,
+  black: black,
+};
